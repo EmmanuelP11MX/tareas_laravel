56183e021d5dd0f418cf465490eecbafaf68b58d
emmanuelp11mx <emmanuelpr10@gmail.com>
2023-09-23 22:19:20 -0600

    Proyecto de tareas basico

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Task;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+Use Illuminate\View\View;
+
+class TaskController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(): View
+    {
+        $tasks = Task::latest()->paginate(3);
+        return view('index',['tasks' => $tasks]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create(): View
+    {
+        return view('create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request): RedirectResponse
+    {
+        //Validación de datos
+        $request->validate([
+            'title' => 'required',
+            'description' => 'required'
+        ]);
+        //Datos que se envian a la base de datos
+        Task::create($request->all());
+        return redirect()->route('tasks.index')->with('success', 'Nueva tarea creada con exito');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Task $task)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Task $task): View
+    {
+        return view('edit',['task' => $task]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Task $task): RedirectResponse
+    {
+        //Agregar validación
+        $task->update($request->all());
+        return redirect()->route('tasks.index')->with('success', 'Nueva tarea Actualizada con exito');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Task $task): RedirectResponse
+    {
+        $task->delete();
+        return redirect()->route('tasks.index')->with('success', 'Tarea Elimanda con exito');
+    }
+}
